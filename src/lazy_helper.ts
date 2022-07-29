66e87c0b32e6b4f963476a94d350f9c01b617d5a
JerrysShan <1670303003@qq.com>
2022-07-29 15:46:15 +0800

    fix: no use object as type

--- src/lazy_helper.ts
+++ src/lazy_helper.ts
@@ -3,9 +3,9 @@ import { Identifier } from './types';
 
 const reflectMethods: ReadonlyArray<keyof ProxyHandler<any>> = ['get', 'set', 'getPrototypeOf'];
 
-function createHandler(delayedObject: () => any): ProxyHandler<object> {
-  const handler: ProxyHandler<object> = {};
-  const install = (name: keyof ProxyHandler<any>) => {
+function createHandler<T extends Object>(delayedObject: () => T): ProxyHandler<T> {
+  const handler: ProxyHandler<T> = {};
+  const install = (name: keyof ProxyHandler<T>) => {
     handler[name] = (...args: any[]) => {
       args[0] = delayedObject();
       const method = Reflect[name];
@@ -17,7 +17,7 @@ function createHandler(delayedObject: () => any): ProxyHandler<object> {
 }
 
 function createProxy<T>(id: Identifier<T>, container: Container): T {
-  const target: object = {};
+  const target: Record<string, any> = {};
   let init = false;
   let value: T;
   const delayedObject: () => T = (): T => {
