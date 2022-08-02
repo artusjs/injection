3c418fc18fd701c9a8e7f53b1a2f89a6bea3fa80
JerrysShan <1670303003@qq.com>
2022-08-02 16:10:48 +0800

    chore: change symbol var

--- src/constant.ts
+++ src/constant.ts
@@ -16,4 +16,4 @@ export const INJECT_HANDLER_PROPS = Symbol.for('injection:handler_props');
 
 export const INJECT_HANDLER_ARGS = Symbol.for('injection:handler_args');
 
-export const LAZY_HANDLER = Symbol('injection:lazy_handler');
+export const LAZY_HANDLER = Symbol.for('injection:lazy_handler');
