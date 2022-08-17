9a8fbb44a53183813027d41c01c845c204991d91
JerrysShan <1670303003@qq.com>
2022-08-17 16:24:31 +0800

    refactor: remove default scope value of injectable

--- src/decorator/injectable.ts
+++ src/decorator/injectable.ts
@@ -1,10 +1,10 @@
-import { InjectableOption, ReflectMetadataType, ScopeEnum } from '../types';
+import { InjectableOption, ReflectMetadataType } from '../types';
 import { recursiveGetMetadata, setMetadata } from '../util';
 import { CLASS_CONSTRUCTOR, CLASS_PROPERTY, INJECT_HANDLER_PROPS, LAZY_HANDLER } from '../constant';
 
 export function Injectable(options?: InjectableOption): ClassDecorator {
   return (target: any) => {
-    const md = { id: target, scope: ScopeEnum.SINGLETON, lazy: false, ...options };
+    const md = { id: target, lazy: false, ...options };
     setMetadata(CLASS_CONSTRUCTOR, md, target);
 
     // make all properties lazy
